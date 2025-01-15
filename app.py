77b1cfdca7225dbe9da16eb9570835f9db47420f
reyhaOilers <noreha.ismail@icu.gov.my>
2025-01-15 10:03:35 +0800

    Initial commit: Flask ML app

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the pre-trained model
+with open('model.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
+
+@app.route('/')
+def home():
+    return "Welcome to the ML API!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json
+    features = np.array(data['features']).reshape(1, -1)
+    prediction = model.predict(features)
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ dockerfile
@@ -0,0 +1,20 @@
+# Use the official Python image
+FROM python
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the requirements file
+COPY requirements.txt .
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code
+COPY . .
+
+# Expose the application port
+EXPOSE 5000
+
+# Run the application
+CMD ["python", "app.py"]
